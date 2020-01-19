e73ce0f1cc3ae2f6fd473ac65a99c75e35d4925d
Kelly 3 <Kelly@Kellys-MacBook-Pro.local>
2020-01-18 19:15:09 -0800

    updated hw5

--- lab02/trig_functions.py
+++ lab02/trig_functions.py
@@ -5,12 +5,12 @@ import math
 def main():
 
     
-    x = float(input("Please enter an angle: "))
-
+    degree = float(input("Please enter an angle: "))
+    x = math.radians(degree) #convert to radians
     cosine = math.cos(x)
     sine = math.sin(x)
 
-    print("The cosine of", x, "is", cosine)
-    print("The sine of", x, "is", sine)
+    print("The cosine of", degree, "is", cosine)
+    print("The sine of", degree, "is", sine)
 
 main()
