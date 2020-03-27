5783204d9308b888e0bafc36cf2c6e3f7d524aef
Kelly 3 <Kelly@Kellys-MBP.fios-router.home>
2020-03-26 23:44:59 -0400

    hw10 added

---
+++ hw10/asteroids_game_starter/game_controller.py
@@ -0,0 +1,173 @@
+from laserbeam import LaserBeam
+from asteroid import Asteroid
+from spaceship import Spaceship
+
+
+class GameController:
+    """
+    Maintains the state of the game
+    and manages interactions of game elements.
+    """
+
+    def __init__(self, SPACE, fadeout):
+        """Initialize the game controller"""
+        self.SPACE = SPACE
+        self.fadeout = fadeout
+
+        self.spaceship_hit = False
+        self.asteroid_destroyed = False
+        self.asteroids = [Asteroid(self.SPACE)]
+        self.laser_beams = []
+        self.spaceship = Spaceship(self.SPACE)
+
+    def update(self):
+        """Updates game state on every frame"""
+        self.do_intersections()
+
+        for asteroid in self.asteroids:
+            asteroid.display()
+        
+         
+        # ======================================================
+        # TODO: Problem 3, Part 2: Laser beam handler
+        # Your code will replace (or augment) the next several
+        # lines. Laser beam objects should remain in the scene
+        # as many frames as their lifespan allows.
+        # Begin problem 3 code changes
+        
+        self.laser_beams = [l for l in self.laser_beams 
+                            if frameCount - l.count < 100]
+
+        for l in range(len(self.laser_beams)):
+            self.laser_beams[l].display()
+
+        # End problem 3, part 2 code changes
+        # =======================================================
+
+        self.spaceship.display()
+
+        # Carries out necessary actions if game over
+        if self.spaceship_hit:
+            if self.fadeout <= 0:
+                fill(1)
+                textSize(30)
+                text("YOU HIT AN ASTEROID",
+                     self.SPACE['w']/2 - 165, self.SPACE['h']/2)
+            else:
+                self.fadeout -= 1
+
+        if self.asteroid_destroyed:
+            fill(1)
+            textSize(30)
+            text("YOU DESTROYED THE ASTEROIDS!!!",
+                 self.SPACE['w']/2 - 250, self.SPACE['h']/2)
+
+    def fire_laser(self, x, y, rot):
+        """Add a laser beam to the game"""
+        x_vel = sin(radians(rot))
+        y_vel = -cos(radians(rot))
+        new_laser_beam = LaserBeam(self.SPACE, x, y, x_vel, y_vel, frameCount)
+        self.laser_beams.append(new_laser_beam)
+
+    def handle_keypress(self, key, keycode=None):
+        if (key == ' '):
+            if self.spaceship.intact:
+                self.spaceship.control(' ', self)
+        if (keycode):
+            if self.spaceship.intact:
+                self.spaceship.control(keycode)
+
+    def handle_keyup(self):
+        if self.spaceship.intact:
+            self.spaceship.control('keyup')
+
+    def do_intersections(self):
+        # ======================================================
+        # TODO: Problem 4, Part 1: Intersections
+        # Here's where you'll probably want to check for intersections
+        # between asteroids and laser beams. Laser beams should be removed
+        # from the scene if they hit an asteroid, and the asteroid should
+        # blow up (the blow_up_asteroid method also must be written. It's
+        # been started for you below).
+        #
+        # The intersection logic below between the spaceship
+        # and asteroids should give a hint as to how this will work.
+        # Begin code changes for Problem 4, Part 1: Intersections
+
+        new_laser_beams = []
+        for lb_index, laser_beam in enumerate(self.laser_beams):
+            for a_index, asteroid in enumerate(self.asteroids):
+                if (abs(laser_beam.x - asteroid.x)
+                    < max(asteroid.radius, laser_beam.radius)
+                    and
+                    abs(laser_beam.y - asteroid.y)
+                    < max(asteroid.radius, laser_beam.radius)):
+                    self.blow_up_asteroid(lb_index, a_index)
+                    break
+            else:
+                new_laser_beams.append(laser_beam)
+            
+        self.laser_beams = new_laser_beams
+                                       
+
+        # End of code changes for Problem 4, Part 1: Intersections
+        # ======================================================
+
+        # If the space ship still hasn't been blown up
+        if self.spaceship.intact:
+            # Check each asteroid for intersection
+            for i in range(len(self.asteroids)):
+                if (
+                      abs(self.spaceship.x - self.asteroids[i].x)
+                      < max(self.asteroids[i].radius, self.spaceship.radius)
+                      and
+                      abs(self.spaceship.y - self.asteroids[i].y)
+                      < max(self.asteroids[i].radius, self.spaceship.radius)):
+                    # We've intersected an asteroid
+                    self.spaceship.blow_up(self.fadeout)
+                    self.spaceship_hit = True
+
+    def blow_up_asteroid(self, lb_index, a_index):
+        # ======================================================
+        # TODO: Problem 4, Part 2: Asteroid blow-up
+
+        # Here you'll write the code to blow up an asteroid.
+        # The parameters represent the indexes for the list of
+        # asteroids and the list of laser beams, indicating
+        # which asteroid is hit by which laser beam.
+
+        # You'll need to:
+        # A) Remove the hit asteroid from the scene
+        # B) Add appropriate smaller asteroids to the scene
+        # C) Make sure that the smaller asteroids are positioned
+        #    correctly and flying off in the correct directions
+
+        # Specifically. If the large asteroid is hit, it should
+        # break into two medium asteroids, which should fly off
+        # perpendicularly to the direction of the laser beam.
+
+        # If a medium asteroid is hit, it should break into three
+        # small asteroids, two of which should fly off perpendicularly
+        # to the direction of the laser beam, and the third
+        # should fly off in the same direction that the laser
+        # beam had been traveling.
+
+        # If a small asteroid is hit, it disappears.
+
+        # Begin code changes for Problem 4, Part 2: Asteroid blow-up
+        blown_asteroid = self.asteroids.pop(a_index)
+        hit_laser_beam = self.laser_beams[lb_index]
+        if blown_asteroid.asize == 'Large':
+            asteroid_med1 = Asteroid(self.SPACE, asize='Med',x=blown_asteroid.x, y=blown_asteroid.y, x_vel=-hit_laser_beam.x_vel/2, y_vel=hit_laser_beam.y_vel/2)
+            self.asteroids.append(asteroid_med1)
+            asteroid_med2 = Asteroid(self.SPACE, asize='Med', x=blown_asteroid.x, y=blown_asteroid.y, x_vel=hit_laser_beam.x_vel/2, y_vel=-hit_laser_beam.y_vel/2)
+            self.asteroids.append(asteroid_med2)
+        if blown_asteroid.asize == 'Med':
+            asteroid_sm1 = Asteroid(self.SPACE, asize='Small',x=blown_asteroid.x, y=blown_asteroid.y, x_vel=-hit_laser_beam.x_vel/2, y_vel=hit_laser_beam.y_vel/2)
+            self.asteroids.append(asteroid_sm1)
+            asteroid_sm2 = Asteroid(self.SPACE, asize='Small', x=blown_asteroid.x, y=blown_asteroid.y, x_vel=hit_laser_beam.x_vel/2, y_vel=-hit_laser_beam.y_vel/2)
+            self.asteroids.append(asteroid_sm2)
+            asteroid_sm3 = Asteroid(self.SPACE, asize='Small', x=blown_asteroid.x, y=blown_asteroid.y, x_vel=hit_laser_beam.x_vel/2, y_vel=hit_laser_beam.y_vel/2)
+            self.asteroids.append(asteroid_sm3)
+        if not self.asteroids:
+            self.asteroid_destroyed = True
